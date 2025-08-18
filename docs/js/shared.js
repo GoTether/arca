8b322631ec0fbde687986a3405f27928e32ec945
GoTether <123700519+GoTether@users.noreply.github.com>
2025-08-18 12:45:01 -0500

    Update shared.js

--- docs/js/shared.js
+++ docs/js/shared.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
   authDomain: 'tether-71e0c.firebaseapp.com',
   databaseURL: 'https://tether-71e0c-default-rtdb.firebaseio.com',
   projectId: 'tether-71e0c',
-  storageBucket: 'tether-71e0c.firebasestorage.app', // <-- UPDATED!
+  storageBucket: 'tether-71e0c.firebasestorage.app', // <-- CORRECT BUCKET!
   messagingSenderId: '277809008742',
   appId: '1:277809008742:web:2586a2b821d8da8f969da7',
   measurementId: 'G-X7ZQ6DJYEN'
